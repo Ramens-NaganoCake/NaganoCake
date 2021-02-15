50ba3353e47a8e0a4a53711471c9c222d0eda8b6
EC2 Default User <ec2-user@ip-172-31-12-21.ec2.internal>
2021-02-15 07:34:35 +0000

    [Add]admins_customer_controller [Add]route_rb customer_controller

---
+++ app/controllers/admins/customers_controller.rb
@@ -0,0 +1,17 @@
+class Admins::CustomersController < ApplicationController
+  
+  def show
+  end
+  
+  def index
+    @customers = Customer.all
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admins do
+   resources :customers, only: [:index, :show, :edit, :update]
    resources :items
    resources :genres
    resources :orders
