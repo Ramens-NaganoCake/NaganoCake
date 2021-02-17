d485a9f2cc96bd307f0eb51a7afdb3b24e409106
EC2 Default User <ec2-user@ip-172-31-12-21.ec2.internal>
2021-02-17 10:12:18 +0000

    [Fix]admin_order_controller/admin_item_order_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,12 +10,23 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
+    item_orders = @order.item_orders
     @order.update(order_params)
+    if @order.order_status === "入金確認"
+      item_orders.each do |item_order|
+        item_order.update(item_order_params)
+      end
+    end
     @orders = Order.all
     render action: :index
   end
 
   private
+
+    def item_order_params
+      params.permit(:maiking_status).merge(maiking_status: 1)
+    end
+
     def order_params
       params.require(:order).permit(:order_status).merge(order_status: params[:order][:order_status].to_i)
     end
