cacff2c167ed828729ff4d27669a8144c3512f61
Yuta <yuta.shogai@gmail.com>
2021-02-18 02:49:50 +0000

    会員側　カート商品 追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,7 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :deliver_destinations, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
