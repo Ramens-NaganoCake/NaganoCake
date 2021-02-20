a6de839b80677009fcaf9d9d109d6cf7c2159336
Ktsunezawa <rapurasnohako@gmail.com>
2021-02-20 06:30:04 +0000

    Fix Order

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,5 @@
 class Item < ApplicationRecord
-<<<<<<< HEAD
-    has_many :cart_items
-    belongs_to :genre
-=======
-
-
+  
   validates :is_active, inclusion: { in: [true, false] }
 
   attachment :image
@@ -16,5 +11,4 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
 
 
->>>>>>> origin/develop
 end
