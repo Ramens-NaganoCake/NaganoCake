1a53a8b2baee4628cef4532af3ba92700511a749
EC2 Default User <ec2-user@ip-172-31-48-63.ec2.internal>
2021-02-20 06:58:09 +0000

    validate

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,9 +1,6 @@
 class CartItem < ApplicationRecord
-<<<<<<< HEAD
-    belongs_to :customer
-    belongs_to :item
-=======
+#
   belongs_to :customer
   belongs_to :item
->>>>>>> origin/develop
+#
 end
