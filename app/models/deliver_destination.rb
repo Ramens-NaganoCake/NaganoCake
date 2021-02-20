e3ab8637e5d5d7c7aaeb22c89342ce0c0a915291
Ktsunezawa <rapurasnohako@gmail.com>
2021-02-20 01:32:43 +0000

    Create Order

--- app/models/deliver_destination.rb
+++ app/models/deliver_destination.rb
@@ -1,16 +1,12 @@
 class DeliverDestination < ApplicationRecord
-<<<<<<< HEAD
-    belongs_to :customer
-    
-	# order/newで使用
-	def order_address
-	  self.postcode + self.address + self.destination
-	end
-=======
   belongs_to :customer
 
   validates :postcode, presence: true, numericality: { only_integer: true }, length: { is: 7 }
   validates :address, presence: true
   validates :destination, presence: true
->>>>>>> origin/develop
+    
+	# order/newで使用
+  def order_address
+	self.postcode + self.address + self.destination
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,12 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-  get 'deliver_destinations/index'
-  get 'deliver_destinations/edit'
-  devise_for :customer, path: :customers,
-     :controllers => {
-=======
   devise_for :customer,
     path: :customers,
     :controllers => {
->>>>>>> origin/develop
       :registrations => "customer/registrations",
       :sessions => "customer/sessions",
       :passwords => "customer/passwords"
     }
-<<<<<<< HEAD
-    
-  devise_for :admin, path: :admins,
-    :controllers => {
-      :sessions => "admin/sessions"
-    }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :admin, path: :admins do
-   resources :items
-   resources :genres
-   resources :orders
-   resources :item_orders
- end
- namespace :customer, path: :customers do
-   resources :cart_items
-   resources :orders
-   resources :delivery_destinations
-   get 'orders/confirm' => 'orders#confirm',　as: 'order_confirm'
-   get 'orders/complete' => 'orders#complete', as: 'order_complete'
- end
-=======
 
   devise_for :admin,
     path: :admins,
@@ -62,9 +34,13 @@ Rails.application.routes.draw do
     resources :cart_items, only: [:index, :update, :create, :destroy]
     delete "cart_items" => "cart_items#all_destroy", as: "cart_item_all_destroy"
 
-    resources :orders, only: [:new, :create, :index, :show]
-    get "orders/confirm" => "orders#confirm", as: "order_confirm"
-    get "orders/complete" => "orders#complete", as: "order_complete"
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post "confirm" 
+        get "complete" 
+      end
+    end
+    
 
     resources :deliver_destinations, only: [:index, :create, :destroy, :edit, :update]
 
@@ -75,5 +51,4 @@ Rails.application.routes.draw do
     patch "update" => "customers#update", as: "customer_update"
   end
   
->>>>>>> origin/develop
 end
